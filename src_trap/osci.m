7fe647183d169d49530cb9382b77b303191d393f
franklin5 <buptphyphy@gmail.com>
2015-08-05 12:26:14 -0500

    temp

--- src_trap/osci.m
+++ src_trap/osci.m
@@ -2,30 +2,41 @@
 
 clear
 clc
-global G
-N = 8;
-Q = 8;
+global G Omega N delta
+N = 1;
+Q = 1;
 qr = 0;
-Omega = 5;
-alist = 0.1:0.5:5;
-%for npara = 1:length(alist)     
-delta = 5;
+Omega = 2;     
+delta = 8/5;
 varepsilon = 0;
 delta_c = 0;
-%kappa = alist(npara);
 kappa = 0;
 G = generateG(N, Q, delta, delta_c, kappa, Omega, qr, varepsilon);
 
-%% We test that the steady state evolution indeed matches the time
-%% evolution results at infinite time
 rho0 = zeros(4*(N+1)^2*Q^2,1);
 rho0(1,1)=1; % initial condition of the state
 maxT = 20;
-[TimeRho, RhoT] = ode45(@timeEvoRHO, [0 maxT], rho0);
-% figure(1)
-% for i = 1:length(RhoT(1,:))
-%     plot(TimeRho,abs(RhoT(:,i)))
-%     hold on
-% end
-% hold off
-plot(TimeRho,abs(RhoT(:,1)))
+[TimeRho1, RhoT] = ode45(@timeEvoRHO, [0 maxT], rho0);
+reduced_rho0 = zeros(4,1);
+reduced_rho0(1,1)=1; % initial condition of the state
+[TimeRho2, reduced_RhoT] = ode45(@reduced_rho_bench, [0 maxT], reduced_rho0);
+t = TimeRho2;
+Delta = sqrt(Omega^2*N+delta^2)/2;
+Cupt = cos(Delta*t)+delta/2/1i/Delta*sin(Delta*t);
+Cdownt = Omega*sqrt(N)/2/1i/Delta*sin(Delta*t);
+rho1 = abs(Cupt).^2;
+rho2 = Cupt.*conj(Cdownt);
+rho3 = conj(Cupt).*Cdownt;
+rho4 = abs(Cdownt).^2;
+figure(1)
+plot(TimeRho1,abs(RhoT(:,1)),'r',TimeRho2,abs(reduced_RhoT(:,1)),'b',t,rho1,'g')
+legend('density matrix','reduced ODE','analytic')
+figure(2)
+plot(TimeRho1,abs(RhoT(:,6)),'r',TimeRho2,abs(reduced_RhoT(:,2)),'b',t,abs(rho2),'g')
+legend('density matrix','reduced ODE','analytic')
+figure(3)
+plot(TimeRho1,abs(RhoT(:,11)),'r',TimeRho2,abs(reduced_RhoT(:,3)),'b',t,abs(rho3),'g')
+legend('density matrix','reduced ODE','analytic')
+figure(4)
+plot(TimeRho1,abs(RhoT(:,16)),'r',TimeRho2,abs(reduced_RhoT(:,4)),'b',t,rho4,'g')
+legend('density matrix','reduced ODE','analytic')
